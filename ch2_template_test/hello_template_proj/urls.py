57cee9c589edb5a8980376cf2da7c342e23b08eb
Syed Zaman Mostafiz <zamanwebdev>
2020-02-16 15:59:25 +0600

    2/15 Python Django Bangla (বাংলা) 2️⃣ Views Template Unit Test Learning with Practice Program

--- ch2_template_test/hello_template_proj/urls.py
+++ ch2_template_test/hello_template_proj/urls.py
@@ -1,4 +1,4 @@
-"""helloworld_proj URL Configuration
+"""hello_template_proj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_app.urls')),
+    path('',include('main_app.urls'))
 ]
