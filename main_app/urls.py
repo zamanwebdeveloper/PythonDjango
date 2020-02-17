7a401c9a44024d9ca377cf30b79c37ccec254c31
Syed Zaman Mostafiz <zamanwebdev>
2020-02-18 00:44:59 +0600

    initialized our project

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homeView, name='home'),
-    path('home/', views.homeView, name='home')
+    path('', views.HomeView.as_view(), name="home"),
+    path('about/', views.AboutView.as_view(), name="about"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,18 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def homeView(request):
-	return HttpResponse("<h1>Hello World</h1>")
+class HomeView(TemplateView):
+	template_name = "index.html"
+	name = "Syed Zaman"
+	country = "Bangladesh"
+	list = [1,2,3,4,5]
+
+class AboutView(TemplateView):
+	template_name = "about.html"
+
+	# def get_context_data(self, **kwargs):
+ #        context = super().get_context_data(**kwargs)
+    
+	# 	 	list = [1,2,3,4,5]
+	# 	 	context['list'] = list
+
+ #        return context
