896d820c17504cf86ad95a8e1ca0a926dafd1c10
Syed Zaman Mostafiz <zamanwebdev>
2020-02-21 00:12:19 +0600

    4/15 Python Django Bangla Tutorial SQL Database Model Learning with Practice Program Completed.

--- ch4_model_admin/quotes/admin.py
+++ ch4_model_admin/quotes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import QuoteCategory
+from . models import Quote
+
+admin.site.register(QuoteCategory)
+admin.site.register(Quote)
--- ch4_model_admin/quotes/views.py
+++ ch4_model_admin/quotes/views.py
@@ -1,4 +1,13 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
-class HomeView(TemplateView):
+from . models import QuoteCategory
+from . models import Quote
+
+class HomeView(ListView):
 	template_name = 'home.html'
+	model = Quote
+
+	def get_queryset(self):
+		query_set = super().get_queryset()
+		return query_set.select_related
+		('quote_category')
